8a033b13cdbc6b9dd742f4f47fb03ce26068ab72
diane <diane.williams@ucdenver.edu>
2022-11-29 13:33:48 -0700

    added comment to angle variable to indicate units are radian

--- games4300/assign2/src/components.h
+++ games4300/assign2/src/components.h
@@ -18,7 +18,7 @@ class CTransform
    public:
       Vec2 pos      = {0.0, 0.0};
       Vec2 velocity  = {0.0, 0.0};
-      float angle    = 0.0f;
+      float angle    = 0.0f;                 // radian
 
       CTransform(const Vec2 &p, const Vec2 &v, float a)
          : pos(p), velocity(v), angle(a) {}
