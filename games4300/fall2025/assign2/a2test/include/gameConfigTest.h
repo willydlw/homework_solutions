1e16eefe1feebe1c67bf4227fb9c1730fb460bc6
willydlw <willydlw@comcast.net>
2025-10-20 10:21:33 -0600

    added tests for missing config types

--- games4300/fall2025/assign2/a2test/include/gameConfigTest.h
+++ games4300/fall2025/assign2/a2test/include/gameConfigTest.h
@@ -19,6 +19,18 @@ class GameConfigTest
     void testRandomBulletConfig();
     void testReadBulletConfig(const char* filename, const BulletConfig& expectedConfig);
 
+    void testFontConfig();
+    void testReadFontConfig(const char* filename, const FontConfig& expectedConfig);
+
+    void testEnemyConfig();
+    void testReadEnemyConfig(const char* filename, const EnemyConfig& expectedConfig);
+
+    void testPlayerConfig();
+    void testReadPlayerConfig(const char* filename, const PlayerConfig& expectedConfig);
+
+    void testWindowConfig();
+    void testReadWindowConfig(const char* filename, const WindowConfig& expectedConfig);
+
     private:
 
     template<typename Visitor>
@@ -33,9 +45,62 @@ class GameConfigTest
         v("outline color red",   a.outlineColor.red , b.outlineColor.red);
         v("outline color green", a.outlineColor.green, b.outlineColor.green);
         v("outline color blue",  a.outlineColor.blue, b.outlineColor.blue);
-        v("outline thickness",   a.outLineThickness, b.outLineThickness);
+        v("outline thickness",   a.outlineThickness, b.outlineThickness);
         v("shape vertices",      a.shapeVertices, b.shapeVertices);
         v("lifespan",            a.lifespan, b.lifespan);
+    }
+
+    template<typename Visitor>
+    void visit_font_members(const FontConfig& a, const FontConfig& b, Visitor v)
+    {
+        v("file name",          a.fileName, b.fileName);
+        v("height",             a.size, b.size);
+        v("color red",          a.color.red, b.color.red);
+        v("color green",        a.color.green, b.color.green);
+        v("color blue",         a.color.blue, b.color.blue);
+    }
+
+
+    template<typename Visitor>
+    void visit_enemy_members(const EnemyConfig& a, const EnemyConfig& b, Visitor v)
+    {
+        v("shapeRadius",        a.shapeRadius, b.shapeRadius);
+        v("collisionRadius",    a.collisionRadius, b.collisionRadius);
+        v("min speed",          a.minSpeed, b.minSpeed);
+        v("max speed",          a.maxSpeed, b.maxSpeed);
+        v("outline color red",  a.outlineColor.red , b.outlineColor.red);
+        v("outline color green",a.outlineColor.green, b.outlineColor.green);
+        v("outline color blue", a.outlineColor.blue, b.outlineColor.blue);
+        v("outline thickness",  a.outlineThickness, b.outlineThickness);
+        v("min vertices",       a.minVertices, b.minVertices);
+        v("max vertices",       a.maxVertices, b.maxVertices);
+        v("small lifespan",     a.smallLifespan, b.smallLifespan);
+        v("spawn interval",     a.spawnInterval, b.spawnInterval);
+    }
+
+    template<typename Visitor>
+    void visit_player_members(const PlayerConfig& a, const PlayerConfig& b, Visitor v)
+    {
+        v("shapeRadius",         a.shapeRadius, b.shapeRadius);
+        v("collisionRadius",     a.collisionRadius, b.collisionRadius);
+        v("speed",               a.speed, b.speed);
+        v("fill color red",      a.fillColor.red, b.fillColor.red);
+        v("fill color green",    a.fillColor.green, b.fillColor.green);
+        v("fill color blue",     a.fillColor.blue, b.fillColor.blue);
+        v("outline color red",   a.outlineColor.red , b.outlineColor.red);
+        v("outline color green", a.outlineColor.green, b.outlineColor.green);
+        v("outline color blue",  a.outlineColor.blue, b.outlineColor.blue);
+        v("outline thickness",   a.outlineThickness, b.outlineThickness);
+        v("shape vertices",      a.shapeVertices, b.shapeVertices);
+    }
+
+    template<typename Visitor>
+    void visit_window_members(const WindowConfig& a, const WindowConfig& b, Visitor v)
+    {
+        v("width",              a.width, b.width);
+        v("height",             a.height, b.height);
+        v("frame rate limit",   a.frameLimit, b.frameLimit);
+        v("full screen mode",   a.fullScreenMode, b.fullScreenMode);
     }
 
     BulletConfig generateRandomBulletConfig();
--- games4300/fall2025/assign2/a2test/src/gameConfigTest.cpp
+++ games4300/fall2025/assign2/a2test/src/gameConfigTest.cpp
@@ -7,18 +7,20 @@
 
 void GameConfigTest::runTests()
 {
-    testBulletConfig(5);
+    testBulletConfig(1);
+    testEnemyConfig();
+    testFontConfig();
+    testPlayerConfig();
+    testWindowConfig();
 }
 
 void GameConfigTest::testBulletConfig(int numRandomTests)
 {   
-    #if 0
     {   
         const char* filename = "configBullet.txt";
         BulletConfig expected = {10, 10, 20, {255, 155, 75}, {50, 90, 150}, 2, 10, 90};
         testReadBulletConfig(filename, expected);
     }
-    #endif
 
     {   // random value testing
         for(int i = 0; i < numRandomTests; i++)
@@ -33,14 +35,16 @@ void GameConfigTest::testRandomBulletConfig()
     // random number testing 
     BulletConfig bc = generateRandomBulletConfig();
 
-    // write to file 
     std::string filename = "randomBulletConfig.txt";
-    std::ofstream outfile(filename, std::ios::out);
-
+    std::string path = GameConfig::CONFIG_DIR_PATH;
+    path.append("/");
+    path.append(filename);
+    
+    std::ofstream outfile(path, std::ios::out);
     if(!outfile.is_open())
     {
         std::cerr << "[FILE OPEN FAILURE] " << __func__ 
-            << ", file name: " << filename << "\n";
+            << ", file name: " << path << "\n";
         std::exit(EXIT_FAILURE);
     }
 
@@ -54,7 +58,7 @@ void GameConfigTest::testRandomBulletConfig()
             << bc.outlineColor.red << " "
             << bc.outlineColor.green << " "
             << bc.outlineColor.blue << " "
-            << bc.outLineThickness << " "
+            << bc.outlineThickness << " "
             << bc.shapeVertices << " "
             << bc.lifespan << "\n";
     outfile.close();
@@ -85,6 +89,7 @@ void GameConfigTest::testReadBulletConfig(const char* filename, const BulletConf
                 << " expected: " << expected << "\n";
             if(errorCount > 1)
             {
+                std::cerr << "\n[ERROR COUNT EXCEEDED] Exiting test\n";
                 std::exit(EXIT_FAILURE);
             }
         }
@@ -97,17 +102,179 @@ BulletConfig GameConfigTest::generateRandomBulletConfig()
     BulletConfig bc;
     RandomNumberGenerator rg;
 
-    bc.shapeRadius = rg.generate(BulletConfig::MIN_SHAPE_RADIUS, BulletConfig::MAX_SHAPE_RADIUS);
+    bc.shapeRadius = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     bc.collisionRadius = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
-    bc.speed = rg.generate(MIN_RANDOM_FLOAT, MAX_RANDOM_FLOAT);
+    // round to two decimal places
+    bc.speed = std::round(rg.generate(MIN_RANDOM_FLOAT, MAX_RANDOM_FLOAT) * 100.0f) / 100.f;
     bc.fillColor.red = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     bc.fillColor.green = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     bc.fillColor.blue = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     bc.outlineColor.red = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     bc.outlineColor.green = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     bc.outlineColor.blue = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
-    bc.outLineThickness = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
+    bc.outlineThickness = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     bc.shapeVertices = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     bc.lifespan = rg.generate(MIN_RANDOM_INT, MAX_RANDOM_INT);
     return bc;
+}
+
+void GameConfigTest::testFontConfig()
+{
+    {   
+        const char* filename = "configFont.txt";
+        FontConfig expected = {"tech.ttf", 24, {255, 242, 100}};
+        testReadFontConfig(filename, expected);
+    }
+}
+
+void GameConfigTest::testReadFontConfig(const char* filename, const FontConfig& expected)
+{
+    static int errorCount = 0;
+
+    GameConfig gc;
+    if(!gc.readConfigFile(filename))
+    {
+        std::cerr << "[FILE OPEN FAILURE] filename: " << filename << "\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    FontConfig actual = gc.getFontConfig();
+    visit_font_members(actual, expected, [](const char* name, auto& actual, auto& expected)
+    {
+        if(actual != expected)
+        {
+            ++errorCount;
+            std::cerr << "[INEQUALITY ERROR] field name: " << name 
+                << ", actual: " << actual << " != "
+                << " expected: " << expected << "\n";
+            if(errorCount > 1)
+            {
+                std::cerr << "\n[ERROR COUNT EXCEEDED] Exiting test\n";
+                std::exit(EXIT_FAILURE);
+            }
+        }
+    });
+}
+
+void GameConfigTest::testEnemyConfig()
+{
+    {   
+        const char* filename = "configEnemy.txt";
+        // 32 32 3 8 255 255 255 2 3 8 90 60
+        EnemyConfig expected = {32, 32, 3, 8, {255, 255, 255}, 2, 3, 8, 90, 60};
+        testReadEnemyConfig(filename, expected);
+    }
+
+}
+
+void GameConfigTest::testReadEnemyConfig(const char* filename, const EnemyConfig& expectedConfig)
+{
+    static int errorCount = 0;
+
+    GameConfig gc;
+    if(!gc.readConfigFile(filename))
+    {
+        std::cerr << "[FILE OPEN FAILURE] filename: " << filename << "\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    
+    EnemyConfig actual = gc.getEnemyConfig();
+    visit_enemy_members(actual, expectedConfig, [](const char* name, auto& actual, auto& expected)
+    {
+        if(actual != expected)
+        {
+            ++errorCount;
+            std::cerr << "[INEQUALITY ERROR] field name: " << name 
+                << ", actual: " << actual << " != "
+                << " expected: " << expected << "\n";
+            if(errorCount > 1)
+            {
+                std::cerr << "\n[ERROR COUNT EXCEEDED] Exiting test\n";
+                std::exit(EXIT_FAILURE);
+            }
+        }
+    });
+}
+
+
+
+void GameConfigTest::testPlayerConfig()
+{
+    {   
+        const char* filename = "configPlayer.txt";
+        PlayerConfig expected = {32, 32, 4, {250, 200, 150}, {255, 255, 255}, 2, 8};
+        testReadPlayerConfig(filename, expected);
+    }
+
+}
+
+void GameConfigTest::testReadPlayerConfig(const char* filename, const PlayerConfig& expectedConfig)
+{
+    static int errorCount = 0;
+
+    GameConfig gc;
+    if(!gc.readConfigFile(filename))
+    {
+        std::cerr << "[FILE OPEN FAILURE] filename: " << filename << "\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    
+    PlayerConfig actual = gc.getPlayerConfig();
+    visit_player_members(actual, expectedConfig, [](const char* name, auto& actual, auto& expected)
+    {
+        if(actual != expected)
+        {
+            ++errorCount;
+            std::cerr << "[INEQUALITY ERROR] field name: " << name 
+                << ", actual: " << actual << " != "
+                << " expected: " << expected << "\n";
+            if(errorCount > 1)
+            {
+                std::cerr << "\n[ERROR COUNT EXCEEDED] Exiting test\n";
+                std::exit(EXIT_FAILURE);
+            }
+        }
+    });
+}
+
+
+void GameConfigTest::testWindowConfig()
+{
+    {   
+        const char* filename = "configWindow.txt";
+        WindowConfig expected = {1280U, 720U, 60, 0};
+        testReadWindowConfig(filename, expected);
+    }
+}
+
+void GameConfigTest::testReadWindowConfig(const char* filename, const WindowConfig& expected)
+{
+    static int errorCount = 0;
+
+    GameConfig gc;
+    if(!gc.readConfigFile(filename))
+    {
+        std::cerr << "[FILE OPEN FAILURE] filename: " << filename << "\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    
+    WindowConfig actual = gc.getWindowConfig();
+    visit_window_members(actual, expected, [](const char* name, auto& actual, auto& expected)
+    {
+        if(actual != expected)
+        {
+            ++errorCount;
+            std::cerr << "[INEQUALITY ERROR] field name: " << name 
+                << ", actual: " << actual << " != "
+                << " expected: " << expected << "\n";
+            if(errorCount > 1)
+            {
+                std::cerr << "\n[ERROR COUNT EXCEEDED] Exiting test\n";
+                std::exit(EXIT_FAILURE);
+            }
+        }
+    });
 }
